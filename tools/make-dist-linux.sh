76071c80024ef80ac2a187d7bd3224617a891668
Ozgur As <ozgur@live.com>
2025-12-23 15:29:44 +0300

    Use standard build directory in Linux distribution script

--- tools/make-dist-linux.sh
+++ tools/make-dist-linux.sh
@@ -7,7 +7,7 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
-BUILD_DIR="${PROJECT_ROOT}/builddir-linux"
+BUILD_DIR="${PROJECT_ROOT}/builddir"
 DIST_DIR="${PROJECT_ROOT}/dist"
 EXE="${BUILD_DIR}/ui-gtk/uget-gtk"
 
